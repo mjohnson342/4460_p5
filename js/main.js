50e58f18ba6c84e147c5fab79ae12a63b6a2467f
tina <christinachung2014@gmail.com>
2017-11-25 18:13:04 -0500

    started implementation of scrolling

--- js/main.js
+++ js/main.js
@@ -1,24 +1,56 @@
-
 // **** Your JavaScript code goes here ****
 
-    var svg = d3.select("svg"),
-        margin = {top: 70, right: 70, bottom: 70, left: 70},
-        width = +svg.attr("width") - margin.left - margin.right,
-        height = +svg.attr("height") - margin.top - margin.bottom;
+///http://vallandingham.me/scroller.html
 
-    var x2 = d3.scaleBand().rangeRound([(width/2) + 40, width]).padding(0.1),
-        x = d3.scaleBand().rangeRound([0, width/2 - 40]).padding(0.1)
-        y = d3.scaleLinear().rangeRound([0,height]);
+var svg = d3.select("svg"),
+    margin = {top: 70, right: 70, bottom: 70, left: 70},
+    width = +svg.attr("width") - margin.left - margin.right,
+    height = +svg.attr("height") - margin.top - margin.bottom;
 
-    var g = svg.append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var x2 = d3.scaleBand().rangeRound([(width/2) + 40, width]).padding(0.1),
+    x = d3.scaleBand().rangeRound([0, width/2 - 40]).padding(0.1)
+    y = d3.scaleLinear().rangeRound([0,height]);
 
-    //aircraft data
-    d3.csv('./data/aircraft_incidents.csv', function(error, datum){
-        if(error) {
-            console.error('error loading coffee_data.csv');
-            console.error(error);
-            return;
-        }
+var g = svg.append("g")
+.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-      });
+
+var sections = document.getElementsByClassName("step");
+sectionPositions = [];
+var startPos;
+
+for (var i = 0, len = sections.length; i < len; i++) {
+    var top = sections[i].getBoundingClientRect().top;
+    console.log(sections[i].getBoundingClientRect().top);
+    console.log(top); //should be same as above, but isnt????
+    if(i === 0) {
+      startPos = top;
+    }
+    sectionPositions.push(top - startPos);
+}
+
+
+
+d3.select(window)
+  .on("scroll.scroller", position);
+
+function position() {
+  var pos = window.pageYOffset - 10;
+  var sectionIndex = d3.bisect(sectionPositions, pos);
+  sectionIndex = Math.min(sections.size() - 1, sectionIndex);
+
+  if (currentIndex !== sectionIndex) {
+    dispatch.active(sectionIndex);
+    currentIndex = sectionIndex;
+  }
+}
+
+//aircraft data
+d3.csv('./data/aircraft_incidents.csv', function(error, datum){
+    if(error) {
+        console.error('error loading coffee_data.csv');
+        console.error(error);
+        return;
+    }
+
+  });
