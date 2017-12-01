4756949af570fcb20cfcee0ed7cf8ac002e29a32
tina <christinachung2014@gmail.com>
2017-12-01 18:13:01 -0500

    started chart1 map

--- js/main.js
+++ js/main.js
@@ -18,6 +18,20 @@ svg.append("rect")
     .attr("height", "100%")
     .attr("fill", "white");
 
+var chart1 = svg.append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+var atlLatLng = new L.LatLng(33.7771, -84.3900);
+var myMap = L.map('vis').setView(atlLatLng, 5);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 10,
+    minZoom: 3,
+    id: 'mapbox.light',
+    accessToken: 'pk.eyJ1IjoiamFnb2R3aW4iLCJhIjoiY2lnOGQxaDhiMDZzMXZkbHYzZmN4ZzdsYiJ9.Uwh_L37P-qUoeC-MBSDteA'
+}).addTo(myMap);
+
 var chart2 = svg.append("g")
 .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
